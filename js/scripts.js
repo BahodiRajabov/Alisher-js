5d30c2e02dcefe83ae0c24a8621298acbce6b093
Bahodir Rajabov <rbcoder12345@gmail.com>
2020-12-10 12:31:32 +0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let elResult = document.querySelector(".result");
+let money = Number(prompt("Alisher qancha jigar bor?"));
+let dollorAmunt = 9433.34;
+let euroAmunt = 10354.03;
+let bilet = 500 * dollorAmunt;
+let hotel = 250 * dollorAmunt;
+let relax = 150 * euroAmunt;
+let totalCoast = bilet + hotel + relax;
+let go = "😎Brat ishlaga omad. Davay bosing soqqa yetadi;🚀 ";
+let earn = "‍👨‍🔧Brat ishlash kerak soqqa kerak💰💰";
+if (money > totalCoast) {
+  elResult.textContent = go;
+} else {
+  elResult.textContent = earn;
+}
